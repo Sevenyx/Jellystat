f35743333a9f2855d16128ae50a9e8e7faa1b564
Sevenyx <38626598+Sevenyx@users.noreply.github.com>
2024-06-04 19:23:27 -0400

    Update tasks.js

--- backend/tasks/tasks.js
+++ backend/tasks/tasks.js
@@ -1,10 +1,8 @@
 const { BackupTask } = require("./BackupTask");
-const { RecentlyAddedItemsSyncTask } = require("./RecentlyAddedItemsSyncTask");
 const { FullSyncTask } = require("./FullSyncTask");
 
 const tasks = {
     FullSyncTask:FullSyncTask,
-    RecentlyAddedItemsSyncTask:RecentlyAddedItemsSyncTask,
     BackupTask:BackupTask,
   };
 module.exports = tasks;
