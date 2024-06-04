c5d7006115fd7f8ae1cea7b09a7e426a1c011667
Sevenyx <38626598+Sevenyx@users.noreply.github.com>
2024-06-04 19:29:28 -0400

    Update tasks.js

--- backend/tasks/tasks.js
+++ backend/tasks/tasks.js
@@ -1,8 +1,6 @@
 const { BackupTask } = require("./BackupTask");
-const { FullSyncTask } = require("./FullSyncTask");
 
 const tasks = {
-    FullSyncTask:FullSyncTask,
     BackupTask:BackupTask,
   };
 module.exports = tasks;
